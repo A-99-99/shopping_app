cbb1f9050a8e24a611de817a629cbd22e91174e3
A-99-99 <aka0worker@gmail.com>
2023-11-19 11:57:16 +0400

    recipe list & recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,11 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is simple test', 'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-master768.jpg?w=1280&q=75')
+    new Recipe('A test recipe1', 'this is simple test', 'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-master768.jpg?w=1280&q=75'),
+    new Recipe('A test recipe2', 'this is simple test', 'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-master768.jpg?w=1280&q=75'),
+    new Recipe('A test recipe3', 'this is simple test', 'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-master768.jpg?w=1280&q=75'),
+    new Recipe('A test recipe4', 'this is simple test', 'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-master768.jpg?w=1280&q=75'),
+    new Recipe('A test recipe5', 'this is simple test', 'https://static01.nyt.com/images/2023/03/19/multimedia/SS-Oven-Roasted-Chicken-Shawarma-phmt/SS-Oven-Roasted-Chicken-Shawarma-phmt-master768.jpg?w=1280&q=75')
   ];
 
   constructor() { }
